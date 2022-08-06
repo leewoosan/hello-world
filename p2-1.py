a7dffc43f59021383f9a7bdabf270e243270ea61
leewoosan <leecw@jnu.ac.kr>
2022-08-06 23:14:23 +0900

    first commit

---
+++ p2-1.py
@@ -0,0 +1,14 @@
+#p2-1.py
+#일괄식 실행 예
+
+print('Welcome to Python world')
+a=5
+b=a+3
+c=a+b
+d=b*c
+e=c/5
+f=c//a
+g=c%a
+h, i = divmod(c, a)
+print("a=",a, "b=",b, "c=",c, "d=",d, "e=",e, "f=",f, "g=",g)
+print('h=',h, 'i=',i, a**d)
